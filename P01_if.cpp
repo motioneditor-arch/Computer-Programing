27f03693f97b1610dd6a546f2b5cce197827d147
muhammadabubakar9200 <muhammadabubakar92367@gmail.com>
2026-07-12 19:01:23 +0500

    Add files via upload

---
+++ P01_if.cpp
@@ -0,0 +1,15 @@
+// 1. Check if a number is positive
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    if (num > 0) {
+        cout << "Positive Number";
+    }
+
+    return 0;
+}
---
+++ P02_if.cpp
@@ -0,0 +1,15 @@
+// 2. Check if a number is divisible by 5
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    if (num % 5 == 0) {
+        cout << "Divisible by 5";
+    }
+
+    return 0;
+}

20c44525172ae7a55b9a99539f6d25c1fdbc5e97
muhammadabubakar9200 <muhammadabubakar92367@gmail.com>
2026-07-13 21:40:42 +0500

    Add files via upload

---
+++ Muhammad Abu Bakar 12.cpp
@@ -0,0 +1,16 @@
+// 12. Check whether a number is positive or negative
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    if (num >= 0)
+        cout << "Positive";
+    else
+        cout << "Negative";
+
+    return 0;
+}

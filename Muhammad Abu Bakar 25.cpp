a5fa474271cbf96efdbc407d4c9fb0533cbda73a
muhammadabubakar9200 <muhammadabubakar92367@gmail.com>
2026-07-13 21:52:26 +0500

    Add files via upload

---
+++ Muhammad Abu Bakar 25.cpp
@@ -0,0 +1,18 @@
+// 25. Check whether a number is positive, negative, or zero
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    if (num > 0)
+        cout << "Positive";
+    else if (num < 0)
+        cout << "Negative";
+    else
+        cout << "Zero";
+
+    return 0;
+}

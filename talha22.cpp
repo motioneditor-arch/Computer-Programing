f8bc97f1777e2318ecd9b6004716adbb7ae05163
talha88-9 <talhamushtaq750@gmail.com>
2026-07-13 20:15:07 +0500

    Add files via upload

---
+++ talha22.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+int num;
+    cout << "\n. Enter a number: ";
+    cin >> num;
+    if (num >= 0) {
+        cout << "Positive \n";
+    } else {
+        cout << "Negative\n";
+    return 0;
+}
+}

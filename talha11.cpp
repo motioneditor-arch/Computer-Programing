2cfd9ac79543c944edbdfdeba3484a0c2e48ec79
talha88-9 <talhamushtaq750@gmail.com>
2026-07-13 19:44:29 +0500

    Add files via upload

---
+++ talha11.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+
+    
+    int password;
+    cout << ". Enter the secret 4-digit password: ";
+    cin >> password;
+    if (password == 1274) {
+        cout << "Access Granted.\n";
+    }
+    return 0;
+}
+    

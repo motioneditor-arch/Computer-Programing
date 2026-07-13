edabcdacf90a58ac10233857d2a513a2120f4948
muhammadabubakar9200 <muhammadabubakar92367@gmail.com>
2026-07-13 21:49:14 +0500

    Add files via upload

---
+++ Muhammad Abu Bakar 23.cpp
@@ -0,0 +1,28 @@
+// 23. Display the day based on a number (1–7)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int day;
+    cin >> day;
+
+    if (day == 1)
+        cout << "Monday";
+    else if (day == 2)
+        cout << "Tuesday";
+    else if (day == 3)
+        cout << "Wednesday";
+    else if (day == 4)
+        cout << "Thursday";
+    else if (day == 5)
+        cout << "Friday";
+    else if (day == 6)
+        cout << "Saturday";
+    else if (day == 7)
+        cout << "Sunday";
+    else
+        cout << "Invalid";
+
+    return 0;
+}

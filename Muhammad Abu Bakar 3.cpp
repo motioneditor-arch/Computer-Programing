ab28f2302f27e19a07add5b9ce6ffc7ae3491810
muhammadabubakar9200 <muhammadabubakar92367@gmail.com>
2026-07-13 20:04:00 +0500

    Add files via upload

---
+++ Muhammad Abu Bakar 3.cpp
@@ -0,0 +1,15 @@
+// 3. Check if a person's age is 18 or above
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age;
+    cin >> age;
+
+    if (age >= 18) {
+        cout << "Adult";
+    }
+
+    return 0;
+}

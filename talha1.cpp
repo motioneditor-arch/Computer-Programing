8c78636b5b8d6fc5b0961ca960b0a47910d9c817
talha88-9 <talhamushtaq750@gmail.com>
2026-07-13 19:23:55 +0500

    Add files via upload

--- talha1.cpp
+++ talha1.cpp
@@ -3,12 +3,11 @@
 using namespace std;
 
 int main() {
-    // 1. [if] Positive Check
-    int num1;
-    cout << "1. Enter an integer: ";
-    cin >> num1;
-    if (num1 > 0) {
-        cout << "The number is positive.\n";
+    int age;
+    cout << ". Enter your age: ";
+    cin >> age;
+    if (age >= 18) {
+        cout << "You are eligible to vote.\n";
     }
     return 0;
 }

ac42fa302852ce581e939ea88738bb33d133ac8f
muhammadabubakar9200 <muhammadabubakar92367@gmail.com>
2026-07-13 21:42:00 +0500

    Add files via upload

---
+++ Muhammad Abu Bakar 13.cpp
@@ -0,0 +1,16 @@
+// 13. Compare two numbers and print the larger one
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    if (a > b)
+        cout << a;
+    else
+        cout << b;
+
+    return 0;
+}

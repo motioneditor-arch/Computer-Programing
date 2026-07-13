e815f22787436918624fd2d2902b554938669321
muhammadabubakar9200 <muhammadabubakar92367@gmail.com>
2026-07-13 21:46:32 +0500

    Add files via upload

---
+++ Muhammad Abu Bakar 21.cpp
@@ -0,0 +1,18 @@
+// 21. Find the largest of three numbers
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a >= b && a >= c)
+        cout << a;
+    else if (b >= a && b >= c)
+        cout << b;
+    else
+        cout << c;
+
+    return 0;
+}

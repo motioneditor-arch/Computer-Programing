488c64b18d54f641bb072f3978bbc123968dd5ce
talha88-9 <talhamushtaq750@gmail.com>
2026-07-13 20:53:19 +0500

    Add files via upload

---
+++ talha36.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int year;
+    cout << ". Enter a year: ";
+    cin >> year;
+    if (year >= 2000) {
+        cout << "Welcome to the 21st century or beyond.\n";
+    }
+}

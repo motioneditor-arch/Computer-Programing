51d501545c6bf1ace4624208b5c3f794461fea79
talha88-9 <talhamushtaq750@gmail.com>
2026-07-13 19:27:25 +0500

    Add files via upload

--- talha3.cpp
+++ talha3.cpp
@@ -3,16 +3,22 @@
 using namespace std;
 
 int main() {
-	int num;
-    cout << "\n. Enter a number: ";
-    cin >> num;
-    if (num > 0) {
-        cout << "Positive\n";
-    } else if (num < 0) {
-        cout << "Negative\n";
+    int age, citizen;
+    cout << "Enter age: ";
+    cin >> age;
+    
+    cout << "Are you a citizen? (1 for citizen, 0 for not): ";
+    cin >> citizen;
+    
+    if (age >= 18) {
+        if (citizen == 1) {
+            cout << "Can Vote\n";
+        } else {
+            cout << "Must be a citizen to vote.\n";
+        }
     } else {
-        cout << "Zero\n";
+        cout << "Too young to vote.\n";
     }
+    
     return 0;
 }
-    

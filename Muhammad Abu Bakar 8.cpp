89cebb872dd41ae895fef42f6de733e2d8198e08
muhammadabubakar9200 <muhammadabubakar92367@gmail.com>
2026-07-13 20:23:30 +0500

    Add files via upload

---
+++ Muhammad Abu Bakar 8.cpp
@@ -0,0 +1,15 @@
+// 8. Check if the temperature is above 30°C
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int temp;
+    cin >> temp;
+
+    if (temp > 30) {
+        cout << "Hot Weather";
+    }
+
+    return 0;
+}

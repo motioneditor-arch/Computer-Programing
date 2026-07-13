56888b041e064fa863f72a936f4d25d3a6b0bb87
muhammadabubakar9200 <muhammadabubakar92367@gmail.com>
2026-07-13 22:08:05 +0500

    Add files via upload

---
+++ Muhammad Abu Bakar 34.cpp
@@ -0,0 +1,28 @@
+// 34. Check Vowel
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+    cin >> ch;
+
+    switch(ch) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+            cout << "Vowel";
+            break;
+        default:
+            cout << "Not Vowel";
+    }
+
+    return 0;
+}

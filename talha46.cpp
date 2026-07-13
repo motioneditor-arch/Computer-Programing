021b7319afd359320e35bac72e1080ae71cadc40
talha88-9 <talhamushtaq750@gmail.com>
2026-07-13 21:15:49 +0500

    Add files via upload

---
+++ talha46.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+int divisor;
+    cout << ". Enter a number: ";
+    cin >> divisor;
+    if (divisor != 0) {
+        cout << "The number is valid for division.\n";
+    }
+    return 0;
+}
+    

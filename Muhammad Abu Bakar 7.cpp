56945d332807bd5e6ea68d445450658ba788a9a5
muhammadabubakar9200 <muhammadabubakar92367@gmail.com>
2026-07-13 20:21:09 +0500

    Add files via upload

---
+++ Muhammad Abu Bakar 7.cpp
@@ -0,0 +1,15 @@
+// 7. Check if a number is even
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    if (num % 2 == 0) {
+        cout << "Even Number";
+    }
+
+    return 0;
+}

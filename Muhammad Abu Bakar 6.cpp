08f3d443980a5fcd54dbeccda57a866d81b6c825
muhammadabubakar9200 <muhammadabubakar92367@gmail.com>
2026-07-13 20:07:44 +0500

    Add files via upload

---
+++ Muhammad Abu Bakar 6.cpp
@@ -0,0 +1,15 @@
+// 6. Check if a student scored full marks (100)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int marks;
+    cin >> marks;
+
+    if (marks == 100) {
+        cout << "Full Marks";
+    }
+
+    return 0;
+}
